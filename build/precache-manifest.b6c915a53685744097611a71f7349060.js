ec1810b9679504d39f3627c2fb07730fd8962bc6
Kishor <kishor.kadam1986@gmail.com>
2020-08-23 16:15:26 +0530

    fist commit

---
+++ build/precache-manifest.b6c915a53685744097611a71f7349060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c912db14bc4cf769bdbc46ddd999fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58e26f0680b71b00df74",
+    "url": "/static/css/main.8838ba68.chunk.css"
+  },
+  {
+    "revision": "0c2f05d22c93c25691ea",
+    "url": "/static/js/2.66799d60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66799d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e26f0680b71b00df74",
+    "url": "/static/js/main.011e7655.chunk.js"
+  },
+  {
+    "revision": "08c49aac8c00567a29d6",
+    "url": "/static/js/runtime-main.59a75140.js"
+  }
+]);
